1b9a51b7e3a86138f0c79c8e6371c4a999e20da3
mcombs289 <maddy.combs4@gmail.com>
2022-10-11 16:19:38 -0400

    carousel

--- client/components/profile/Profile.js
+++ client/components/profile/Profile.js
@@ -59,6 +59,43 @@ export class Profile extends React.Component {
             {favoriteTv ? <img src={favoriteTv.imageUrl} alt="image" /> : null}
           </Col>
         </Row>
+        <Row>
+          <div
+            id="carouselExampleControls"
+            class="carousel slide"
+            data-bs-ride="carousel"
+          >
+            <div class="carousel-inner">
+              <div class="carousel-item active">1</div>
+              <div class="carousel-item">2</div>
+              <div class="carousel-item">3</div>
+            </div>
+            <button
+              class="carousel-control-prev"
+              type="button"
+              data-bs-target="#carouselExampleControls"
+              data-bs-slide="prev"
+            >
+              <span
+                class="carousel-control-prev-icon"
+                aria-hidden="true"
+              ></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button
+              class="carousel-control-next"
+              type="button"
+              data-bs-target="#carouselExampleControls"
+              data-bs-slide="next"
+            >
+              <span
+                class="carousel-control-next-icon"
+                aria-hidden="true"
+              ></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
+        </Row>
       </Container>
     );
   }
