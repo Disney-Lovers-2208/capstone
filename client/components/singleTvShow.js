3967a00fc20a956ab66061b5fa5bbd7d7fd2d25c
Kourtney Smith <kourtneysmith@Kourtneys-MacBook-Pro.local>
2022-10-12 10:36:30 -0400

    Created single tv show component

---
+++ client/components/singleTvShow.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Col, Row } from 'react-bootstrap';
+import TvCard from './TvCard';
+
+export const SingleTvShow = () => {
+    const tvshow = useSelector(())
+}
