413fa1b2220d8b773abf383ed4f52f67a6d5b991
Vanessa Gao <vgao825@gmail.com>
2022-10-16 15:27:07 -0400

    added stars into activity log

--- client/components/singleProduct/ReviewForm.js
+++ client/components/singleProduct/ReviewForm.js
@@ -2,16 +2,21 @@ import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Container, Row, Col } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
-import Stars from "../Stars";
+import RatingStars from "./RatingStars";
 
 export function ReviewForm(props) {
+  const dispatch = useDispatch();
+  let navigate = useNavigate();
+  const [starRating, setStarRating] = useState("");
+  const [comments, setComments] = useState("");
+
   return (
     <Container>
       <h1>Leave a Review!</h1>
       <hr />
       <Row>
         <form className="review-form">
-          <Stars />
+          <RatingStars />
           <div>
             <label htmlFor="comments">Comments</label>
             <textarea />
