13d719a7bb0d711a944255bdb851718303ed9d8c
mcombs289 <maddy.combs4@gmail.com>
2022-10-24 00:10:50 -0400

    little changes

--- client/App.js
+++ client/App.js
@@ -48,6 +48,7 @@ const App = () => {
       {isLoggedIn ? (
         <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route exact path="/searchfor/" element={<Home />} />
           <Route path="/home" element={<Home />} />
           <Route path="*" element={<PageNotFound />} />
           <Route path="/profile" element={<UserProfilePage />} />
--- client/components/search/SearchBar.js
+++ client/components/search/SearchBar.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import { Col, Row, Button } from "react-bootstrap";
-import { GoSearch } from "react-icons/go";
+import { BsSearch } from "react-icons/bs";
 import { fetchTvShows } from "../../store/tvshows";
 import { fetchMovies } from "../../store/movies";
 import { fetchBooks } from "../../store/books";
@@ -43,22 +43,27 @@ export const SearchBar = () => {
     <>
       <Row>
         <Col style={{ display: "flex", flexDirection: "row" }}>
-            <input
-              type="search"
-              placeholder="Search for..."
-              onChange={(evt) => setSearch(evt.target.value)}
-              value={search}
-              className="search-bar"
-              onKeyDown={handleKeyDown}
-            />
+          <input
+            type="search"
+            placeholder="Search for..."
+            onChange={(evt) => setSearch(evt.target.value)}
+            value={search}
+            className="search-bar"
+            onKeyDown={handleKeyDown}
+          />
           <Button
             type="submit"
-            variant="light"
             as={Link}
             to={`/searchfor/${search}`}
-            style={{ padding: "6px", borderRadius: "10px" }}
+            style={{
+              padding: "6px",
+              borderRadius: "10px",
+              backgroundColor: "transparent",
+              border: "none",
+              color: "#03045e",
+            }}
           >
-            <GoSearch />
+            <BsSearch size={30} />
           </Button>
         </Col>
       </Row>
