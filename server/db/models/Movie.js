e5e9cb90c9a0c3069395a0a13c15e8608a88e012
Vanessa Gao <vgao825@gmail.com>
2022-10-25 11:10:15 -0400

    heroku changes

--- server/db/models/Movie.js
+++ server/db/models/Movie.js
@@ -11,19 +11,12 @@ const Movie = db.define("movie", {
   },
   description: {
     type: Sequelize.TEXT,
-    validate: {
-      notEmpty: true,
-    },
   },
   genre: {
     type: Sequelize.ARRAY(Sequelize.TEXT),
   },
   imageUrl: {
     type: Sequelize.STRING,
-    allowNull: false,
-    validate: {
-      notEmpty: true,
-    },
     defaultValue:
       "https://media.istockphoto.com/vectors/poster-template-with-retro-banner-design-for-presentation-concert-vector-id996101744?b=1&k=20&m=996101744&s=612x612&w=0&h=fHQFQ0stKZm0yagV9i0aYUdH2Wje-Js19-AqRSxhZ3k=",
   },
